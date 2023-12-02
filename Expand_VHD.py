e87f216ca2b5a191adfb04084d7a8c1460b718f0
Decem6er <cgmz@live.com>
2023-12-02 23:11:00 +0800

    Use GB

--- Expand_VHD.py
+++ Expand_VHD.py
@@ -6,9 +6,9 @@ def change_vhdx_size():
         print("文件不存在，请重新输入正确的地址！")
         return
     
-    new_size = input("请输入新的虚拟磁盘大小（单位：MB）：")
+    new_size = input("请输入新的虚拟磁盘大小（单位：GB）：")
     try:
-        new_size = int(new_size)
+        new_size = int(new_size) * 1024  # 转换为 MB
     except ValueError:
         print("输入的大小不合法，请重新输入！")
         return
